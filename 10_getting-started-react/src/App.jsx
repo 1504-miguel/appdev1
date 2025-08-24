63f90c48fc17f83d4ad3504f8664829f4e772e52
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-08-24 21:25:40 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>&#128513;</h1>
+        <h1>About Me</h1>
+      </div>
+      <h2>Nathaniel Miguel C. David</h2>
+      <h3>Bachelor of Science in Information Systems 3-A</h3>
+      <br></br>
+      <h3>My name Nathaniel Miguel David is in the Bible.</h3>
+      <br></br>
+      <h3>I want to learn React so I can get better at coding. Like I said before, I am not really good at it yet, and sometimes I wonder how to make or fix code. Learning React will help me understand it more.</h3>
+
+      
+      
+      
+    </>
+  )
+}
+
+export default App
