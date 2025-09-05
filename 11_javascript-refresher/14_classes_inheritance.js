8dfa4be90a49f6eb6e7176db2040fbbc4cc58751
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-09-05 23:25:09 +0800

    Add classes and inheritance activity

---
+++ 11_javascript-refresher/14_classes_inheritance.js
@@ -0,0 +1,25 @@
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+
+const person1 = new Person("Miguel");
+person1.sayHello();
+
+const student1 = new Student("Miguel David");
+student1.sayHello(); 
+student1.study();    
