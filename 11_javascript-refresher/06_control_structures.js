d072792069e35402fa2faf7570b86481442fee3a
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-09-05 23:17:53 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,32 @@
+const score = 83
+let grade
+
+if (score>=90) {
+    grade = "A"
+} 
+else if (score>=80){
+    grade = "B"
+} 
+else if (score>= 70){
+    grade = "C"
+} 
+else {
+    grade = "F"
+}
+
+console.log(`Grade:${grade}`)
+
+
+
+
+
+for (let i =1; i<= 5; i++){
+    console.log(i)
+}
+
+let count = 0
+
+while (count<3){
+    console.log("Hello")
+    count++
+}
