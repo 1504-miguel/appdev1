0a47875b29f65f4669f90de6cab36d031ad19258
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-09-05 23:21:17 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,18 @@
+let name = "Nathaniel"; 
+let age = 20;
+
+console.log("Before:", name, age);
+
+
+name = "Nathaniel Miguel David";
+age = 21;
+console.log("After reassignment with let:", name, age);
+
+
+const birthYear = 2004;
+console.log("Birth Year:", birthYear);
+
+
+var city = "Macabebe";
+city = "Pampanga";
+console.log("Using var:", city);
