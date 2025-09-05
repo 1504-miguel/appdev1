5ef476b40fac378da61fe220312242b7e96adc28
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-09-05 23:12:57 +0800

    Add objects activity

---
+++ 11_javascript-refresher/04_objects.js
@@ -0,0 +1,14 @@
+const aboutMe = {
+    name: "Nathaniel",
+    age: 21,
+    course: "BSIS",
+    introduce: function(){
+        console.log(`Hi my name is ${this.name} and I am ${this.age} years old.`)
+
+    
+    }
+}
+
+aboutMe.introduce()
+
+aboutMe.hobby = "Walking"
