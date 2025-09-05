7749195056aa1b7561f7546c26289d22c36490f5
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-09-05 23:00:06 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,12 @@
+const name = "Nathaniel Miguel David"
+let age = 21
+const isStudent = true
+
+
+console.log(1+2)
+console.log(3-2)
+console.log(6*2)
+console.log(3/2)
+
+console.log("5" == 5)
+console.log("5" === 5)
