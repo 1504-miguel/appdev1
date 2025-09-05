a8b29727b02560da6ad921af1dd6115d587cea55
Miguel David-nc <miguelcastrodavid@gmail.com>
2025-09-05 23:21:57 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,15 @@
+
+const greet = (name) => {
+  return "Hello, " + name;
+};
+console.log(greet("Nathaniel"));
+
+
+const square = n => n * n; 
+console.log(square(5));
+
+
+const sayHi = () => {
+  console.log("Hi!");
+};
+sayHi();
