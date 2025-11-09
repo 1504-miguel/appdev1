cd6074f04c13ea24b9ec4d9080646b84c5affbfe
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2025-11-09 21:21:06 +0800

    Done with using a component

--- react-your-first-component/src/App.jsx
+++ react-your-first-component/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 
 function Profile() {
-  return <img src="https://i.pravatar.cc/100" alt="Profile" />;
+  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Profile" />;
 }
 
 export default function App() {
   return (
     <div>
       <h1>My First Component</h1>
+      <Profile />
+      <Profile />
+      <Profile />
     </div>
   );
 }
+
+
