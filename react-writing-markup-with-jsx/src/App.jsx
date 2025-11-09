5ac963154a94d0aa901b8e1a7b828dda6ccef44a
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2025-11-09 22:00:21 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 
+
+
 function App() {
   return (
-    <div>
-      <h1>My Profile</h1>
-      <img src="avatar.jpg" className="profile" />
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
+      />
       <ul>
         <li>Invent new traffic lights</li>
         <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
+        <li>Improve the spectrum technology</li>
       </ul>
-    </div>
+    </>
   );
 }
+
+
 export default App
