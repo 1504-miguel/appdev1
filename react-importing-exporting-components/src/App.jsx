e6dc692db7ba5e8b2527a65841d0f1cebdecc5c7
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2025-11-09 21:41:25 +0800

    Done with exporting and importing multiple components from the same file

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Gallery from './Gallery';
+import { Profile, Bio, Avatar } from './Profile';
 
 function App() {
   return (
     <div>
       <h1>Welcome to My App</h1>
       <Gallery />
+      <Profile />
+      <Bio />
+      <Avatar />
     </div>
   );
 }
---
+++ react-importing-exporting-components/src/Profile.jsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return <p>Profile Component</p>;
+}
+
+export function Bio() {
+  return <p>Bio Component</p>;
+}
+
+export function Avatar() {
+  return <p>Avatar Component</p>;
+}
