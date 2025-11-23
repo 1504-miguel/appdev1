908fe7aff5e75d196b6e1ddf3667dbc5025ffcd2
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2025-11-23 20:14:12 +0800

    Adding import USER, POST, TODOS Lists

--- react-useeffect-simple-api-fetch/src/main.jsx
+++ react-useeffect-simple-api-fetch/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TodoList from './UsersFetch'
-
+import UserList from './UsersFetch'
+import PostList from './PostsFetchAsync'
+import TodoList from './TodosFetchAxios'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <UserList />
+    <PostList />
     <TodoList />
   </StrictMode>,
 )
