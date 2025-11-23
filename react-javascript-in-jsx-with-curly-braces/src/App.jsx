72d8886aad156fb09efe34526cbcaaaa2436abe7
Nathaniel Miguel David <miguelcastrodavid@gmail.com>
2025-11-23 21:02:45 +0800

    Using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,9 +1,29 @@
-export default function Avatar() {
+// export default function Avatar() {
+//   return (
+//     <img
+//       className="avatar"
+//       src="https://www.manilatimes.net/manilatimes/uploads/images/2025/08/15/714241.jpg"
+//       alt="TJ AND KZ"
+//     />
+//   );
+// }
+
+export default function TodoList() {
+  const name = 'TJ and KZ';
+  
   return (
+    <>
+    <h1>{name}'s Photo</h1>
+
     <img
-      className="avatar"
-      src="https://www.manilatimes.net/manilatimes/uploads/images/2025/08/15/714241.jpg"
-      alt="TJ AND KZ"
-    />
+        className="avatar"
+        src="https://www.manilatimes.net/manilatimes/uploads/images/2025/08/15/714241.jpg"
+        alt={name}
+      />
+
+    
+    </>
+    
+    
   );
 }
